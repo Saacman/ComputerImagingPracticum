ce4a9df4e49e0ae87f79423d448a7ac383511861
Isaac Sanchez <saacman@pop-os.localdomain>
2020-03-16 10:11:38 -0700

    Added clips functionality

--- gigavisor/routes.py
+++ gigavisor/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, url_for, flash, redirect, jsonify, redirect, 
 from gigavisor import app, socketio, db, bcrypt
 from gigavisor.image import filterIt
 from PIL import Image as im
-from gigavisor.forms import RegistrationForm, LoginForm
+from gigavisor.forms import RegistrationForm, LoginForm, PostForm
 from gigavisor.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_socketio import send
@@ -104,4 +104,42 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    os.remove("gigavisor"+post.clip)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
